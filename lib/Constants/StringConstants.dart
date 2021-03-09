17402a278f6d1c99a33808d5247eb32eb41f0913
chirag  mevada <chiragtech9teen@gmail.com>
2021-03-09 17:52:51 +0530

    Keval

---
+++ lib/Constants/StringConstants.dart
@@ -0,0 +1,11 @@
+const String API_URL = "https://watcher03.herokuapp.com/";
+const String Access_Token = "RvHiQ6J4QJoAMeA0ysCw-HJklmBHklmnknNJn-hghJUdksjH";
+
+class Session {
+  static const String memberId = "MemberId";
+  static const String memberNo = "MemberNo";
+  static const String deviceType = "deviceType";
+  static const String userRole = "userRole";
+  static const String societyId = "SocietyId";
+  static const String societyCode = "societyCode";
+}
---
+++ lib/Data/Services.dart
@@ -0,0 +1,74 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:watcher_app_for_user/Constants/StringConstants.dart';
+import 'package:watcher_app_for_user/Data/ClassList/ResponseClass.dart';
+
+Dio dio = new Dio();
+
+class Services {
+  static Future<List> postForList({@required apiName, body}) async {
+    String url = API_URL + "$apiName";
+    var header = Options(
+      headers: {
+        "authorization": "$Access_Token" // set content-length
+      },
+    );
+    var response;
+    try {
+      if (body == null) {
+        response = await dio.post(url, options: header);
+      } else {
+        response = await dio.post(url, data: body, options: header);
+      }
+      if (response.statusCode == 200) {
+        List dataList = [];
+        var responseData = response.data;
+        if (responseData["IsSuccess"] == true &&
+            responseData["Data"].length > 0) {
+          dataList = responseData["Data"];
+        }
+        return dataList;
+      } else {
+        debugPrint("error" + response.data.toString());
+        throw Exception("error->" + response.data.toString());
+      }
+    } catch (e) {
+      debugPrint("Catch Error-> ${e.toString()}");
+      throw Exception("Catch Error->${e.toString()}");
+    }
+  }
+
+  static Future<ResponseDataClass> postForSave(
+      {@required apiName, body}) async {
+    String url = API_URL + "$apiName";
+    var header = Options(
+      headers: {
+        "authorization": "$Access_Token" // set content-length
+      },
+    );
+    var response;
+    try {
+      if (body == null) {
+        response = await dio.post(url, options: header);
+      } else {
+        response = await dio.post(url, data: body, options: header);
+      }
+      if (response.statusCode == 200) {
+        ResponseDataClass responseData = new ResponseDataClass(
+            Message: "No Data", IsSuccess: false, Data: 0.toString());
+        var data = response.data;
+        responseData.Message = data["message"];
+        responseData.IsSuccess = data["IsSuccess"];
+        responseData.Data = data["Data"].toString();
+
+        return responseData;
+      } else {
+        print("error ->" + response.data.toString());
+        throw Exception(response.data.toString());
+      }
+    } catch (e) {
+      print("Catch error -> ${e.toString()}");
+      throw Exception(e.toString());
+    }
+  }
+}
