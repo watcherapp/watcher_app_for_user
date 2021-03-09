6999fa94ac9f5ac2171efc35f0153b4f67461ae2
chirag  mevada <chiragtech9teen@gmail.com>
2021-03-09 13:56:53 +0530

    megha

--- lib/Modules/UserApp/Screens/UserHomeScreen.dart
+++ lib/Modules/UserApp/Screens/UserHomeScreen.dart
@@ -18,7 +18,7 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
   @override
   void initState() {
     _controller = VideoPlayerController.network(
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
     );
 
     _initializeVideoPlayerFuture = _controller.initialize();
